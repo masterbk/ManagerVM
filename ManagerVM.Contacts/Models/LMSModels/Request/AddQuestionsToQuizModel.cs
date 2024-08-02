379e68a8164a344684c08adf24ecd88208cabc1c
Cao Văn Thúy <thuybk23@gmail.com>
2024-08-02 17:48:55 +0700

    .

---
+++ ManagerVM.Contacts/Models/LMSModels/Request/AddQuestionsToQuizModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerVM.Contacts.Models.LMSModels.Request
+{
+    /// <summary>
+    /// wsfunction: local_cms_api_course_module_add_questions_to_quiz
+    /// </summary>
+    public class AddQuestionsToQuizModel
+    {
+        public long? moduleid { get; set; }
+        /// <summary>
+        /// String mảng id Question
+        /// </summary>
+        public string questionids { get; set; }
+    }
+}
